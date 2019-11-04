0f1cdf8c9bc7e53c4c9d4b7a9048da3a74315d1b
ljfirst <ljfirst@mail.ustc.edu.cn>
2019-11-04 15:53:13 +0800

    modify structure

---
+++ liujun/DataStructure/3_stackHeapQueue/heap/a.java
@@ -0,0 +1,11 @@
+package heap;
+
+/**
+ * @author liujun
+ * @version 1.0
+ * @date£º 2019-11-04 15:45
+ * @author¡ªEmail:ljfirst@mail.ustc.edu.cn
+ * @description:
+ */
+public class a {
+}
---
+++ liujun/DataStructure/3_stackHeapQueue/queue/a.java
@@ -0,0 +1,11 @@
+package queue;
+
+/**
+ * @author liujun
+ * @version 1.0
+ * @date£º 2019-11-04 15:46
+ * @author¡ªEmail:ljfirst@mail.ustc.edu.cn
+ * @description:
+ */
+public class a {
+}
