40ee5b64b27ff1ae6eded8d6433bac4e7e1cb4d4
ljfirst <ljfirst@mail.ustc.edu.cn>
2020-01-09 19:17:25 +0800

    ClockwiseSpiralMatrix

--- java/DataStructure/line/array/twoDimensionalArray/twoDimensionalArrayApply/ClockwiseSpiralMatrix.java
+++ java/DataStructure/line/array/twoDimensionalArray/twoDimensionalArrayApply/ClockwiseSpiralMatrix.java
@@ -1,11 +1,68 @@
 package DataStructure.line.array.twoDimensionalArray.twoDimensionalArrayApply;
 
+import Logic.check.MatrixCheck;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author liujun
  * @version 1.0
  * @date： 2019-11-09 22:22
  * @author—Email:liujunfirst@outlook.com
- * @description:
+ * @description: 顺时针螺旋矩阵
  */
 public class ClockwiseSpiralMatrix {
+
+    public int[] print(int[][] matrix) {
+
+        if (!MatrixCheck.judgeisRectangle(matrix)) {
+            return null;
+        }
+
+        List<Integer> list = new ArrayList<Integer>();
+        //左上角(a, b),右下角(c, d)
+        int a = 0;
+        int b = 0;
+        int c = matrix.length - 1;
+        int d = matrix[0].length - 1;
+        int[] target = new int[(c+1)*(d+1)];
+        while (a <= c && b <= d) {
+            //最外层一圈打印
+            //特殊情况：单独行、单独列
+            if (a == c) {
+                for (int i = b; i <= d; i++) {
+                    list.add(matrix[a][i]);
+                }
+            }else if (b == d) {
+                for (int i = a; i <= c; i++) {
+                    list.add(matrix[i][b]);
+                }
+            }else {
+                //四个循环，分别是从左到右，从上到下，到右到左，从下到上
+                for (int i = b; i < d; i++) {
+                    list.add(matrix[a][i]);
+                }
+                for (int i = a; i < c; i++) {
+                    list.add(matrix[i][d]);
+                }
+                for (int i = d; i > b; i--) {
+                    list.add(matrix[c][i]);
+                }
+                for (int i = c; i > a; i--) {
+                    list.add(matrix[i][b]);
+                }
+            }
+            //缩小一层
+            a++;
+            b++;
+            c--;
+            d--;
+        }
+        for (int i = 0; i < list.size(); i++) {
+            target[i] = list.get(i);
+        }
+        //target = list.toArray(new int[0]);
+        return target;
+    }
 }
--- java/UnitTest/datastructureTest/lineTest/arrayTest/twoDimensionalArrayTest/applyTest/ClockwiseSpiralMatrixTest.java
+++ java/UnitTest/datastructureTest/lineTest/arrayTest/twoDimensionalArrayTest/applyTest/ClockwiseSpiralMatrixTest.java
@@ -1,5 +1,10 @@
 package UnitTest.datastructureTest.lineTest.arrayTest.twoDimensionalArrayTest.applyTest;
 
+import DataStructure.line.array.twoDimensionalArray.twoDimensionalArrayApply.ClockwiseSpiralMatrix;
+import org.junit.Test;
+
+import java.util.Arrays;
+
 /**
  * @author liujun
  * @version 1.0
@@ -8,4 +13,43 @@ package UnitTest.datastructureTest.lineTest.arrayTest.twoDimensionalArrayTest.ap
  * @description:
  */
 public class ClockwiseSpiralMatrixTest {
+
+    ClockwiseSpiralMatrix clockwiseSpiralMatrix = new ClockwiseSpiralMatrix();
+
+    @Test
+    public void Testprint() {
+        int[] target;
+        target = clockwiseSpiralMatrix.print(Demo01);
+        Arrays.equals(target, Answer01);
+        target = clockwiseSpiralMatrix.print(Demo02);
+        Arrays.equals(target, Answer02);
+        target = clockwiseSpiralMatrix.print(Demo03);
+        Arrays.equals(target, Answer03);
+        target = clockwiseSpiralMatrix.print(Demo04);
+        Arrays.equals(target, Answer04);
+    }
+
+    //Demo
+    int[][] Demo01 = {
+            {1,  2, 3,4},
+            {12,13,14,5},
+            {11,16,15,6},
+            {10, 9, 8,7}
+    };
+    int[] Answer01 = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+    int[][] Demo02 = {
+            { 2, 3,4},
+            {11,12,5},
+            {10,13,6},
+            { 9, 8,7}
+    };
+    int[] Answer02 = {2,3,4,5,6,7,8,9,10,11,12,13};
+    int[][] Demo03 = {
+            {2,  3, 4,5},
+            {11,12,13,6},
+            {10, 9, 8,7}
+    };
+    int[] Answer03 = {2,3,4,5,6,7,8,9,10,11,12,13};
+    int[][] Demo04 = {};
+    int[] Answer04 = null;
 }
