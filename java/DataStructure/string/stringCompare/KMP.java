1a4a92ec4781fd4b3d96cd258e13109c2d094111
ljfirst <ljfirst@mail.ustc.edu.cn>
2020-02-02 00:23:11 +0800

    KMP

--- java/DataStructure/string/stringCompare/KMP.java
+++ java/DataStructure/string/stringCompare/KMP.java
@@ -3,66 +3,60 @@ package DataStructure.string.stringCompare;
 import DataStructure.string.StringCompare;
 
 /**
- *@author liujun
- *@date： 2019-7-8 下午04:57:00
- *@author—Email:liujunfirst@outlook.com
- *@CSDN Blog URL:https://blog.csdn.net/ljfirst/article/details/95072616
- *@description: 设计并实现KMP算法
- *@version 1.0
+ * @author liujun
+ * @version 1.0
+ * @date： 2019-7-8 下午04:57:00
+ * @author—Email:liujunfirst@outlook.com
+ * @CSDN Blog URL:https://blog.csdn.net/ljfirst/article/details/95072616
+ * @description: 设计并实现KMP算法
  */
 public class KMP implements StringCompare {
-	@Override
-	public boolean compare(String source, String target) {
-		return check(source, target) ? kmp(source, target) : false;
-	}
-	//kmp 匹配过程
-	public boolean kmp(String source, String pattern) {
+    @Override
+    public boolean compare(String source, String target) {
+        return check(source, target) ? kmp(source, target) : false;
+    }
 
-		//源串指针和目的串指针
-		int sourcePoint = 0;
-		int patternPoint = 0;
-		int[] next = getNext(pattern);
+    //kmp 匹配过程
+    public boolean kmp(String source, String pattern) {
 
-		while (sourcePoint < source.length() && patternPoint < pattern.length()) {
-			if(patternPoint == -1 || source.charAt(sourcePoint) == pattern.charAt(patternPoint)){
-				sourcePoint++;
-				patternPoint++;
-			}else{
-				patternPoint = next[patternPoint];
-			}
-		}
+        //源串指针和目的串指针
+        int sourcePoint = 0;
+        int patternPoint = 0;
+        int[] next = getNext(pattern);
 
-		if(patternPoint == pattern.length()){
-			return true;
-		}
-		return false;
-	}
+        while (sourcePoint < source.length() && patternPoint < pattern.length()) {
+            if (patternPoint == -1 || source.charAt(sourcePoint) == pattern.charAt(patternPoint)) {
+                sourcePoint++;
+                patternPoint++;
+            } else {
+                patternPoint = next[patternPoint];
+            }
+        }
 
-	public static int[] getNext(String s) {
-
-		int[] next = new int[s.length()];
-		getNext(s, next);
-		return next;
-	}
+        if (patternPoint == pattern.length()) {
+            return true;
+        }
+        return false;
+    }
 
 	//获取next数组:寻找最长前缀
-	public static void getNext(String s, int[] next) {
+    public int[] getNext(String s) {
 
+        int[] next = new int[s.length()];
 		int point = 0;
-		int pointmatch = -1;
+		int prefix = -1;
 		next[0] = -1;
-		int slength = s.length();
-		
-		while (point < slength - 1) {
-			if(pointmatch == -1 || s.charAt(pointmatch) == s.charAt(point)){
+
+		while (point < s.length() - 1) {//因为求解最后一位的匹配值，所有此处是 <
+			if (prefix == -1 || s.charAt(prefix) == s.charAt(point)) {
 				point++;
-				pointmatch++;
-				next[point] = pointmatch;
-			}else{
-				pointmatch = next[pointmatch];
+				prefix++;
+				next[point] = prefix;
+			} else {
+				prefix = next[prefix];
 			}
 		}
-	}
-	
+        return next;
+    }
 
 }
