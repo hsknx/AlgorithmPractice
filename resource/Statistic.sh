f310a7de883894f70571d341013d6b3d5e29ab14
ljfirst <ljfirst@mail.ustc.edu.cn>
2020-04-12 17:03:25 +0800

    update readme.md

--- resource/Statistic.sh
+++ resource/Statistic.sh
@@ -1,18 +1,20 @@
 
 echo "Algorithm代码量统计"
-dirpath=/d/workspace/AlgorithmPractice/java
+dirpath=/d/workspace/AlgorithmPractice/javaVersion
 
 Algorithm=$(find  $dirpath/Algorithm/     -iname "*.java" |grep -v Test |wc -l)
 DataStructure=$(find $dirpath/DataStructure/ -iname "*.java" |grep -v Test |wc -l)
+Design=$(find $dirpath/DesignPattern/  -iname "*.java" |grep -v Test |wc -l)
 Logic=$(find $dirpath/Logic/  -iname "*.java" |grep -v Test |wc -l)
 
 AlgorithmUT=$(find $dirpath/UnitTest/algorithmTest     -iname "*.java" |grep  Test |wc -l)
 DataStructureUT=$(find $dirpath/UnitTest/datastructureTest -iname "*.java" |grep  Test |wc -l)
+DesignUT=$(find $dirpath/UnitTest/DesignPatternTest    -iname "*.java" |grep  Test |wc -l)
 LogicUT=$(find $dirpath/UnitTest/logicTest        -iname "*.java" |grep  Test |wc -l)
 
 
-Total=$((Algorithm+DataStructure+Logic))
-TotalUT=$((AlgorithmUT+DataStructureUT+LogicUT))
-echo "       Total  - Algorithm  - DataStructure  -  Logic"
-echo "Code:  $Total    -    $Algorithm      -    $DataStructure          -    $Logic"
-echo "UT  :  $TotalUT     -    $AlgorithmUT      -    $DataStructureUT          -    $LogicUT"
+Total=$((Algorithm+DataStructure+Design+Logic))
+TotalUT=$((AlgorithmUT+DataStructureUT+DesignUT+LogicUT))
+echo "      Total - Algorithm  - DataStructure  - DesignPattern  -  Logic"
+echo "Code: $Total   -    $Algorithm      -    $DataStructure          -     $Design         -    $Logic"
+echo "UT  : $TotalUT    -    $AlgorithmUT      -    $DataStructureUT          -     $DesignUT          -    $LogicUT"
